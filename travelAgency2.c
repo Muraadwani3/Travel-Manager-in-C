143a951aee2b2180f4a1f7ded87e63ec515c5300
Muraadwani3 <105493065+Muraadwani3@users.noreply.github.com>
2022-05-16 15:46:42 +0530

    Add files via upload

---
+++ travelAgency2.c
@@ -0,0 +1,65 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+struct  driver
+{
+	char name[50];
+	char dlNo[50];
+	char route[50];
+	int kms;
+ };
+	
+	int main(){
+		struct driver d1,d2,d3;
+	printf("Enter the details of the drivers:\n\n");
+	printf("Name of the first driver:\n");
+		scanf("%s", &d1.name);
+		
+	printf("Licence number of the first driver:\n");
+		scanf("%s", &d1.dlNo);
+		
+	printf("Route travelled by first driver:\n");
+		scanf("%s", &d1.route);
+		
+	printf("Kms travelled by first driver:\n");
+		scanf("%d", &d1.kms);
+		
+		
+		
+	printf("\nName of the second driver:\n");
+		scanf("%s", &d2.name);
+		
+	printf("Licence number of the second driver:\n");
+		scanf("%s", &d2.dlNo);
+		
+	printf("Route travelled by second driver:\n");
+		scanf("%s", &d2.route);
+		
+	printf("Kms travelled by second driver:\n");
+		scanf("%d", &d2.kms);
+		
+		
+	printf("\nName of the third driver:\n");
+		scanf("%s", &d3.name);
+		
+	printf("Licence number of the third driver:\n");
+		scanf("%s", &d3.dlNo);
+		
+	printf("Route travelled by third driver:\n");
+		scanf("%s", &d3.route);
+		
+	printf("Kms travelled by third driver:\n");
+		scanf("%d", &d3.kms);
+		
+		
+		
+		
+		
+		
+		
+		return 0;
+		}
+		
+		
+		
+		
